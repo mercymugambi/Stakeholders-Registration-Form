633c8a84cc01b08f1a8d1cf1b0e79f492f5119d2
Peter Kirwa <peter.kirwa@bsl.co.ke>
2019-04-13 12:11:46 +0300

    Added the county, subcounty, ward DAO

---
+++ src/co/ke/bsl/model/County.java
@@ -0,0 +1,33 @@
+package co.ke.bsl.model;
+
+public class County {
+	private String spd_county_id;
+	private String name;
+	/**
+	 * @return the spd_county_id
+	 */
+	public String getSpd_county_id() {
+		return spd_county_id;
+	}
+	/**
+	 * @param spd_county_id the spd_county_id to set
+	 */
+	public void setSpd_county_id(String spd_county_id) {
+		this.spd_county_id = spd_county_id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ src/co/ke/bsl/model/SubCounty.java
@@ -0,0 +1,34 @@
+package co.ke.bsl.model;
+
+public class SubCounty {
+	
+	private String spd_subcounty_id;
+	private String name;
+	/**
+	 * @return the spd_subcounty_id
+	 */
+	public String getSpd_subcounty_id() {
+		return spd_subcounty_id;
+	}
+	/**
+	 * @param spd_subcounty_id the spd_subcounty_id to set
+	 */
+	public void setSpd_subcounty_id(String spd_subcounty_id) {
+		this.spd_subcounty_id = spd_subcounty_id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
